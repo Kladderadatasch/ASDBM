abefa61ee8501b3eec9e813b038fe5d0c7c96f9b
Maximilian Bakenhus <s1788901@baltic05.geos.ed.ac.uk>
2019-02-11 14:39:00 +0000

    tidying

--- AJAX.js
+++
@@ -1,9 +0,0 @@
-function loadDoc() {\n\
-var xhttp = new XMLHttpRequest();\n\
-xhttp.onreadystatechange = function() {\n\
-  if (this.readyState == 4 && this.status == 200) {\n\
-   document.getElementById("demo").innerHTML = this.responseText;\n\
-  }\
-};\
-xhttp.open("GET", "ajax_info.txt", true);\
-xhttp.send();}
